356562fb791637c511d87d5adf6896b6a4dc23e2
Amirshah98760 <amirshah98760@gmail.com>
2024-09-22 11:56:04 +0500

    Get request added

--- index.mjs
+++ index.mjs
@@ -4,3 +4,17 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+// Array to store data 
+let userArray = [];
+
+
+const findUserById = (id) =>{\
+    return userArray.find(user => user.id === id);
+}
+
+// Get request to fetch all users 
+
+app.get('/user', (req, res)=>{
+res.status(200).json(userArray);
+});
